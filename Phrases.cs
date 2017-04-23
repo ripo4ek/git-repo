3fe789f1a29c46f5d722db27186e9264d440b0b7
Immortal <Immortal@DESKTOP-JMO6HF1>
2017-04-23 15:13:48 +0300

    Добавьте файлы проекта.

---
+++ Phrases.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autocomplete
+{
+    public class Phrases : IReadOnlyList<string>
+    {
+        private readonly string[] verbs;
+        private readonly string[] adjectives;
+        private readonly string[] nouns;
+
+        public Phrases(string[] verbs, string[] adjectives, string[] nouns)
+        {
+            this.verbs = verbs;
+            this.adjectives = adjectives;
+            this.nouns = nouns;
+        }
+
+        // Это называется вычисляемое свойство с геттером.
+        public virtual int Length
+        {
+            get { return verbs.Length*adjectives.Length*nouns.Length; }
+        }
+
+        // Это называется индексатор c геттером. Он позволяет писать так var x = phrases[i];
+        public virtual string this[int index]
+        {
+            get
+            {
+                if (index < 0) throw new IndexOutOfRangeException("index = " + index);
+                var ni = index%nouns.Length;
+                var ai = index/nouns.Length%adjectives.Length;
+                var vi = index/(nouns.Length*adjectives.Length)%verbs.Length;
+                return verbs[vi] + " " + adjectives[ai] + " " + nouns[ni];
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (int i = 0; i < Length; i++)
+                yield return this[i];
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Size: {3}. Verbs: {0}, Adjectives: {1}, Nouns: {2}", verbs.Length, adjectives.Length,
+                nouns.Length, Length);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count { get { return Length; } }
+    }
+}
---
+++ RightBorderTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Autocomplete
+{
+    public class RightBorderTask
+    {
+        /// <returns>
+        /// Возвращает индекс правой границы. То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
+        /// Если такого нет, то возвращает items.Length
+        /// </returns>
+        /// <remarks>
+        /// Функция должна быть НЕ рекурсивной
+        /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+        /// </remarks>
+        public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, long left, long right)
+        {
+            // IReadOnlyList похож на List, но у него нет методов модификации списка.
+            // Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
+           
+            while (left < right)
+            {
+                long middle = (right + left) / 2;
+                if (string.Compare(prefix, phrases[(int)middle], StringComparison.OrdinalIgnoreCase) > 0)
+                    left = middle;
+                else right = middle + 1;
+            }
+
+            return (int)left;
+        }
+
+    }
+}
